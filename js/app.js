b3ad327726f602c081d1e23424773a28cb38da5c
Mashiyyat Delos Santos <delossantos.mashiyyat@gmail.com>
2021-11-14 20:46:51 +0800

    New Version

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    margin: 15,
+    center: false,
+    dotsEach: true,
+    dots: true,
+    center: true,
+    responsive: {
+      0:{
+          items:1.1,
+      },
+      600:{
+          items: 2,
+      },
+      1000:{
+          items:1.8,
+      }
+    }
+});
+
+AOS.init()
